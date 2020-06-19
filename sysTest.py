f3bff425c3414a14c5d2a9dba6d2a1e7c55a8009
Dmitry Berestnev <dimka@bdima.ru>
2020-06-19 14:35:44 +0400

    test with new library

--- sysTest.py
+++ sysTest.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 '''
- Название: Программа определяющая производительность узла theool;
+ Название: Программа для определения производительности узла theool;
  Автор: Берестнев Дмитрий Дмитриевич;
- Требования: python3, pip3, gcc, python3-dev;
- Библиотеки: pp-ez, psutil;
+ Требования: python3, pip3, gcc, python3-dev, smartmontools;
+ библиотеки требующие устрановки: pp-ez, psutil;
 '''
 
 import os
@@ -17,6 +17,7 @@ import random
 import string
 
 class sysTest:
+	#точка монтирования дисков перед проверкой
 	__mount_point = "/mnt/sys_test/"
 	#массив разделов защищенных от записи
 	__partition_protection = ["sda1"]
@@ -94,7 +95,6 @@ class sysTest:
 		self.__test_result["cpu"]['freq']['current'] = freq.current
 		self.__test_result["cpu"]['freq']['min'] = freq.min
 		self.__test_result["cpu"]['freq']['max'] = freq.max
-
 		self.__test_result["cpu"]["all_cores"] = all_cores
 		self.__test_result["cpu"]["cores"] = cores
 
@@ -108,18 +108,22 @@ class sysTest:
 
 		for disk in dev_arr:
 			disk_info = {
-				"type": None,
 				"smart": False,
-				"serial": None,
+				"serial_number": None,
 				"device": None,
 				"size": None,
-				"time_write": None,
-				"time_read": None,
 				"partitions": {},
 			}
 			obg_d = re.search("^sd[a-z]{1}$", disk)
 			if(obg_d != None):
 				disk_info["device"] = disk
+				disk_full_info = subprocess.Popen(["smartctl","-i","/dev/" + disk], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+				d_out, d_err = disk_full_info.communicate()
+				d_out_obj = self.__comandResultToObject(d_out)
+
+				disk_info["serial_number"] = d_out_obj["Serial Number"]
+				disk_info["size"] = int(float(re.search("\d{0,},{0,}\d{0,} GB",d_out_obj["User Capacity"]).group(0).split(" ")[0].strip().replace(",",".")))
+				disk_info["smart"] = self.__smartTest(disk)
 				for partition in dev_arr:
 					partition_info = {}
 					if(re.match("^" + disk + "[0-8]{1}$", partition)):
@@ -137,17 +141,15 @@ class sysTest:
 								partition_write_speed = subprocess.Popen(['dd', "if=/dev/zero",'of=' + self.__mount_point + partition + "/" + temp_file,'bs=1M','count=1',"conv=fdatasync"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
 								pw_out,ps_err = partition_write_speed.communicate()
-								pw_out = str(pw_out)
-								pw_out = re.sub(".{0,} copied, ", "", pw_out)
-								pw_out = re.split(" s, ", pw_out)[0]
+								pw_out = self.__parseSpeed(pw_out)
+								
 								partition_info["write_speed"] = pw_out
 
 								#попытка чтения
 								partition_read_speed = subprocess.Popen(['dd', "if=" + self.__mount_point + partition + "/" + temp_file,'of=/dev/null','bs=1M','count=1'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 								pr_out,ps_err = partition_read_speed.communicate()
-								pr_out = str(pr_out)
-								pr_out = re.sub(".{0,} copied, ", "", pr_out)
-								pr_out = re.split(" s, ", pr_out)[0]
+								pr_out = self.__parseSpeed(pr_out)
+
 								partition_info["read_speed"] = pr_out
 
 								os.system("rm "+ self.__mount_point + partition + "/" + temp_file)
@@ -159,6 +161,34 @@ class sysTest:
 						os.system("umount " + self.__mount_point + partition)
 						disk_info["partitions"][partition] = partition_info
 				self.__test_result["disks"].append(disk_info)
+
+	def __comandResultToObject(self, result):
+		result = str(result)
+		result = result.split("\\n")
+		result = result[4:]
+		result = result[0:-2]
+		result_obj = {}
+		i = 0;
+		for param in result:
+			key_value = param.split(":")
+			result_obj[key_value[0]] = key_value[1].strip()
+			i = i + 1
+		return result_obj
+
+	def __parseSpeed(self, speed_byt):
+		speed_str = str(speed_byt)
+		speed_str = re.sub(".{0,} copied, ", "", speed_str)
+		speed_str = re.split(" s, ", speed_str)[0]
+		return speed_str
+
+	def __smartTest(self, disk):
+		smart_comand = subprocess.Popen(["smartctl", "-H", "/dev/" + disk], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+		s_out,s_err = smart_comand.communicate()
+		s_out_obj = self.__comandResultToObject(s_out)
+
+		if(s_out_obj["SMART overall-health self-assessment test result"] == "PASSED"):
+			return True
+		return False
 
 	#возвращение данных о железе в виде объекта
 	def getObj(self):
