2803735337fef52d72e27a1c60466d62210ddd7e
Dmitry Berestnev <dimka@bdima.ru>
2020-07-04 20:15:25 +0400

    server v1.1.0 (with mongodb)

--- server.py
+++ server.py
@@ -11,8 +11,9 @@ import socket
 import subprocess
 import pp
 import pickle
-import datetime
+from datetime import datetime
 from pymongo import MongoClient
+from select import select
 
 class metaServer:
 
@@ -24,9 +25,16 @@ class metaServer:
 	__db_name = None
 	__db_collection = None
 
+	__db = None
 	__db_connection = None
+	__db_posts = None
 
-	def __init__(self, socket_port = 9090, queue = 999, db_ip = "127.0.0.1", db_port = 27018, db_name = "meta-database", db_collection = "meta-collection"):
+	__to_monitor = []
+
+	__client_socket = None
+	__addr = None
+
+	def __init__(self, socket_port = 9090, queue = 999, db_ip = "127.0.0.1", db_port = 27018, db_name = "meta-database-test", db_collection = "meta-collection-test"):
 		if(type(socket_port is int) and type(queue is int) and type(db_ip is str) and type(db_port is int) and type(db_name is str) and type(db_collection is str)):
 			self.__socket_port = socket_port
 			self.__queue = queue
@@ -35,41 +43,69 @@ class metaServer:
 			self.__db_name = db_name
 			self.__db_collection = db_collection
 		else:
-			raise Exception("metaServer в конструктор переданных данные не верных типов")
+			raise Exception("metaServer - invalid types passed to the constructor")
 
 	#Запуск сервера
 	def start(self):
-		self.__startDbServer()
+		self.startDbServer()
 
-		self.__db_connection()
+		self.db_connection()
 
-		__db = None
+		server_socket = socket.socket()
+		server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		server_socket.bind(("",self.__socket_port))
+		server_socket.listen(self.__queue)
 
-		sock = socket.socket()
-		sock.bind(("",self.__socket_port))
+		self.__to_monitor = [server_socket]
+
 		print("socket start")
 
 		while True:
-			sock.listen(self.__queue)
-			conn, addr = sock.accept()
 
-			print("connected", addr)
+			for s_err in self.__to_monitor: #фикс бага с сокетом имеющим отрицательный дескриптор, если дескриптор отрицательный - удаляем сокет из масива сокетов
+				if s_err.fileno() == -1:
+					self.__to_monitor.remove(s_err)
 
-			while True:
-				data = conn.recv(1024)
-				if not data:
-					break
-				conn.send("success".encode())
-				data_obj = pickle.loads(data)
-				data_obj["IP"] = addr[0]
-				self.__post(data_obj)
-			conn.close()
+			ready_to_read, _, _ = select(self.__to_monitor, [], []) #read, write, errors
+
+			for sock in ready_to_read:
+				if sock is server_socket:
+					self.__accept_connection(sock)
+				else:
+					self.__read_message(sock)
+
+	def __read_message(self, client_socket):
+		data = client_socket.recv(1024)
+
+		if data:
+			client_socket.send("success".encode())
+			data_obj = pickle.loads(data)
+			data_obj["IP"] = self.__addr
+			data_obj["time_stamp"] = datetime.today().timestamp()
+			self.__post(data_obj)
+			print("Data fron user: " + self.__addr + " received")
+		else:
+			client_socket.close()
+			print("Connection whith user: " + self.__addr + " is broken")
+
+	def __accept_connection(self, server_socket):
+		self.__client_socket, addr = server_socket.accept()
+		self.__addr = addr[0]
+
+		self.__to_monitor.append(self.__client_socket)
+
+		print("Connection with user: " + self.__addr + " established")
 
 	def __post(self, obj):
-		posts = self.__db.posts
-		post_id = posts.insert_one(obj).inserted_id
-		for post in posts.find():
-			pp(post)
+		post_id = self.__db_posts.update({"IP": obj["IP"]}, obj, upsert = True)
+
+	#вывод всех записей в DB
+	def showDB(self):
+		if(self.__db_posts != None):
+			for post in self.__db_posts.find():
+				pp(post)
+		else:
+			raise Exception("metaServer - it is impossible to request information output from DB without connecting to it, please call db_connection () function first")
 
 	#Уставновка порта для прослушивания
 	def setPort(self, port):
@@ -85,10 +121,13 @@ class metaServer:
 			return True
 		return False
 
-	def __db_connection(self):
+	#подключение к DB
+	def db_connection(self):
 		client = MongoClient(self.__db_ip, self.__db_port)
 		self.__db = client[self.__db_name]
 		self.__db_collection = self.__db[self.__db_collection]
+		self.__db_posts = self.__db.posts
+
 		print("Db connection")
 
 	#Установка IP базы данных
@@ -119,9 +158,14 @@ class metaServer:
 			return True
 		return False
 
-	def __startDbServer(self):
+	#Запуск сервера DB
+	def startDbServer(self):
 		result = subprocess.Popen(["mongod","--port", str(self.__db_port)], stdout=subprocess.PIPE)
+
 		print("Db server start")
 
+'''Пример использования'''
 server = metaServer()
 server.start()
+#server.db_connection()
+#server.showDB()
